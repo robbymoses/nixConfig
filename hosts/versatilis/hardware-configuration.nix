d83ba67164f0f8759564ef113fbaf81a946ec500
Robert Moses <rmoses@titaniumbytes.com>
2025-08-29 21:52:12 -0500

    remove dupe line

--- hosts/versatilis/hardware-configuration.nix
+++ hosts/versatilis/hardware-configuration.nix
@@ -37,5 +37,4 @@
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
